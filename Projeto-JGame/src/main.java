e94fe38b7eb03114f5a3c3e609f24927eff9b3be
JoseNeto5689 <jose.sicupiraneto@gmail.com>
2021-05-16 22:11:52 -0300

    Repositório criado com projeto base

---
+++ Projeto-JGame/src/main.java
@@ -0,0 +1,13 @@
+import jgame.*;
+public class main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        
+    }
+    
+}
